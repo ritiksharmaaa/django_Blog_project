3ec514681c7da8ce973f3a50d56b429ee06663a4
ritik sharma <111228529+ritiksharmaaa@users.noreply.github.com>
2025-01-02 02:50:55 +0530

    Update urls.py
    in url import setting

--- Blog/urls.py
+++ Blog/urls.py
@@ -19,6 +19,8 @@ from django.urls import path , include
 from Blogpost import views 
 from django.conf.urls.static import static
 
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path("__debug__/", include("debug_toolbar.urls")),
