3db12510b527b369e87c7cbd33ad3e7c02666e7a
Ritik sharma <ritik1832004@gmail.com>
2025-01-02 12:15:43 +0530

    add staitc file dir

--- Blog/settings.py
+++ Blog/settings.py
@@ -139,7 +139,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = "https://simple-django-blog.onrender.com/static/"
+
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 # Default primary key field type
